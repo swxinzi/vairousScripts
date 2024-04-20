1938d9c8830b3817ee110bffb4be4a1185daa1dc
previous <2831207404@qq.com>
2024-04-20 13:21:40 +0800

    增加京东软件重写脚本

---
+++ rewrite/jingdong/jingdong.js
@@ -0,0 +1,9 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("unctionId=basic")) {
+    if (obj?.JDMiaoSha){
+        delete obj.JDMiaoSha;
+    }
+}
