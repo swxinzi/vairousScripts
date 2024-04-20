7650a200e3ba930357ab1e34b65a31e863a45c84
previous <2831207404@qq.com>
2024-04-20 13:43:17 +0800

    优化京东软件重写脚本

--- rewrite/jingdong/jingdong.js
+++ rewrite/jingdong/jingdong.js
@@ -2,34 +2,30 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("unctionId=basic")) {
-    if (obj?.JDMiaoSha){
-        delete obj.JDMiaoSha;
-    }
-}else if (url.includes("functionId=welcomeHome")) {
-    if (obj?.floorList?.length > 0) {
-        const delItems = [
-            "bottomXview", // 底部悬浮通栏推广
-            "float", // 悬浮推广小圆图
-            "photoCeiling", // 顶部通栏动图推广
-            // "recommend", // 为你推荐
-            "ruleFloat", // 资质与规则
-            "searchIcon", // 右上角消费券
-            "topRotate", // 左上角logo
-            "tabBarAtmosphere" // 底部悬浮通栏推广
-        ];
-        // 首页 图层列表
-        obj.floorList = obj.floorList.filter((i) => !delItems?.includes(i?.type));
-    }
-    // 首页 顶部背景图
-    // if (obj?.topBgImgBig) {
-    //   delete obj.topBgImgBig;
+if (url.includes("functionId=welcomeHome")) {
+    // if (obj?.floorList?.length > 0) {
+    //     const delItems = [
+    //         "bottomXview", // 底部悬浮通栏推广
+    //         "float", // 悬浮推广小圆图
+    //         "photoCeiling", // 顶部通栏动图推广
+    //         // "recommend", // 为你推荐
+    //         "ruleFloat", // 资质与规则
+    //         "searchIcon", // 右上角消费券
+    //         "topRotate", // 左上角logo
+    //         "tabBarAtmosphere" // 底部悬浮通栏推广
+    //     ];
+    //     // 首页 图层列表
+    //     obj.floorList = obj.floorList.filter((i) => !delItems?.includes(i?.type));
     // }
-    // 首页 下拉二楼
-    if (obj?.webViewFloorList?.length > 0) {
-        obj.webViewFloorList = [];
-    }
-
+    // // 首页 顶部背景图
+    // // if (obj?.topBgImgBig) {
+    // //   delete obj.topBgImgBig;
+    // // }
+    // // 首页 下拉二楼
+    // if (obj?.webViewFloorList?.length > 0) {
+    //     obj.webViewFloorList = [];
+    // }
+    obj = {};
 }
 
 $done({ body: JSON.stringify(obj) });
