4cf1477413d663d8e589243076e6aae37282aa0a
previous <2831207404@qq.com>
2025-02-28 17:34:49 +0800

    loon增加去广告脚本

--- scripts/DaMai-script.js
+++ scripts/DaMai-script.js
@@ -1,9 +1,11 @@
 const url = $request.url;
 if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
+let bodyStr = $response.body;
+let result = '';
 
 if (url.includes("/mtop.damai.mec.aristotle")) {
     // 首页
+    let obj = JSON.parse(bodyStr);
     if (obj?.data?.nodes?.length > 0) {
         let newNodes = obj.data.nodes;
         for (let node of newNodes){
@@ -14,8 +16,25 @@ if (url.includes("/mtop.damai.mec.aristotle")) {
         }
 
         obj.data.nodes = newNodes;
+        result = JSON.stringify(obj);
     }
+}else if (url.includes("mtop.film.pfusercenter.identity.membercenter.get")){
+    // 会员中心
+    let bodyJsonStr = bodyStr.replace("mtopjsonp1" , "");
+    bodyJsonStr     = bodyJsonStr.substring(1,bodyJsonStr.length - 1);
+    let obj         = JSON.parse(bodyJsonStr);
+
+    if (obj?.data?.layers?.length > 0?){
+        let newLayerList = obj.data.layers;
+        for ( let layer of newLayerList){
+            if ( layer?.layerId.includes("35e80789-a55c-4dcc-88e2-6989d3ad298f")){
+                layer.sections = [];
+            }
+        }
+    }
+
+    result = 'mtopjsonp1(' + JSON.stringify(obj) + ')';
 }
 
 
-$done({ body: JSON.stringify(obj) });
+$done({ body: result });
