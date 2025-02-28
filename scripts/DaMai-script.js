57421e793c31462e7c6c5e11f1cd81d41114e9e8
previous <2831207404@qq.com>
2025-02-28 17:49:10 +0800

    loon增加去广告脚本

--- scripts/DaMai-script.js
+++ scripts/DaMai-script.js
@@ -1,6 +1,6 @@
 const url = $request.url;
 if (!$response.body) $done({});
-let obj = JSON.parse(bodyStr);
+let obj = JSON.parse($response.body);
 
 if (url.includes("/mtop.damai.mec.aristotle")) {
     // 首页
