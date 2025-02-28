53efe2f1310e385328e97a04849b6900bef6b915
previous <2831207404@qq.com>
2025-02-28 15:57:09 +0800

    loon增加去广告脚本

--- scripts/DaMai-script.js
+++ scripts/DaMai-script.js
@@ -7,7 +7,8 @@ if (url.includes("/mtop.damai.mec.aristotle")) {
     if (obj?.data?.nodes?.length > 0) {
         let newNodes = obj.data.nodes;
         for (let node of newNodes){
-            if ( node?.data?.nodeId?.includes("948087aa-f753-4367-944f-bf470adf2d8f")){
+            if ( node?.data?.nodeId?.includes("948087aa-f753-4367-944f-bf470adf2d8f") ||
+            node?.data?.nodeId?.includes("fb3cf36c-ed3f-4787-b20d-fc59ded4ea2d")){
                 node.nodes = [];
             }
         }
