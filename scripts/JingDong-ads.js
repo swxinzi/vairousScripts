a49acda13ca391e23ca73c72e744d76d82a20e9a
previous <2831207404@qq.com>
2025-03-07 16:54:15 +0800

    增加京东去广告脚本

---
+++ scripts/JingDong-ads.js
@@ -0,0 +1,36 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("functionId=readCustomSurfaceList")) {
+    // 底部导航栏   黑暗模式
+    if (obj?.data?.result?.modeMap?.dark?.navigationAll.length > 0) {
+        let newNavigationList = [];
+        let navigationList = obj.data.result.modeMap.dark.navigationAll;
+        for (let item of navigationList){
+            if (item.functionId != 'index' || item.functionId != 'cart' || item.functionId != 'home') {
+                continue;
+            }
+            newNavigationList.push(item);
+        }
+
+        obj.data.result.modeMap.dark.navigationAll = newNavigationList;
+    }
+
+    // 底部导航栏   普通模式
+    if (obj?.data?.result?.modeMap?.normal?.navigationAll.length > 0) {
+        let newNavigationList = [];
+        let navigationList = obj.data.result.modeMap.normal.navigationAll;
+        for (let item of navigationList){
+            if (item.functionId != 'index' || item.functionId != 'cart' || item.functionId != 'home') {
+                continue;
+            }
+            newNavigationList.push(item);
+        }
+
+        obj.data.result.modeMap.normal.navigationAll = newNavigationList;
+    }
+}
+
+
+$done({ body: JSON.stringify(obj) });
