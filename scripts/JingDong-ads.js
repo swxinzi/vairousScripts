e79a7f07a398f4fa4e32a1541fe47fe90e8a7533
previous <2831207404@qq.com>
2025-03-07 17:07:05 +0800

    优化京东去广告脚本

--- scripts/JingDong-ads.js
+++ scripts/JingDong-ads.js
@@ -4,7 +4,7 @@ let obj = JSON.parse($response.body);
 
 if (url.includes("functionId=readCustomSurfaceList")) {
     // 底部导航栏   黑暗模式
-    if (obj?.data?.result?.modeMap?.dark?.navigationAll.length > 0) {
+    if (obj?.result?.modeMap?.dark?.navigationAll.length > 0) {
         let newNavigationList = [];
         let navigationList = obj.data.result.modeMap.dark.navigationAll;
         for (let item of navigationList){
@@ -14,11 +14,11 @@ if (url.includes("functionId=readCustomSurfaceList")) {
             newNavigationList.push(item);
         }
 
-        obj.data.result.modeMap.dark.navigationAll = newNavigationList;
+        obj.result.modeMap.dark.navigationAll = newNavigationList;
     }
 
     // 底部导航栏   普通模式
-    if (obj?.data?.result?.modeMap?.normal?.navigationAll.length > 0) {
+    if (obj?.result?.modeMap?.normal?.navigationAll.length > 0) {
         let newNavigationList = [];
         let navigationList = obj.data.result.modeMap.normal.navigationAll;
         for (let item of navigationList){
@@ -28,7 +28,7 @@ if (url.includes("functionId=readCustomSurfaceList")) {
             newNavigationList.push(item);
         }
 
-        obj.data.result.modeMap.normal.navigationAll = newNavigationList;
+        obj.result.modeMap.normal.navigationAll = newNavigationList;
     }
 }
 
