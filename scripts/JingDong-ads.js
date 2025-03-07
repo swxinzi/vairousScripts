5e4bae201717bcbd234d58d3e3df51ac453ba1aa
previous <2831207404@qq.com>
2025-03-07 17:12:01 +0800

    优化京东去广告脚本

--- scripts/JingDong-ads.js
+++ scripts/JingDong-ads.js
@@ -4,18 +4,18 @@ let obj = JSON.parse($response.body);
 
 if (url.includes("functionId=readCustomSurfaceList")) {
     // 底部导航栏   黑暗模式
-    if (obj?.result?.modeMap?.dark?.navigationAll.length > 0) {
-        let newNavigationList = [];
-        let navigationList = obj.result.modeMap.dark.navigationAll;
-        for (let item of navigationList){
-            if (item.functionId != 'index' || item.functionId != 'cart' || item.functionId != 'home') {
-                continue;
-            }
-            newNavigationList.push(item);
-        }
-
-        obj.result.modeMap.dark.navigationAll = newNavigationList;
-    }
+    // if (obj?.result?.modeMap?.dark?.navigationAll.length > 0) {
+    //     let newNavigationList = [];
+    //     let navigationList = obj.result.modeMap.dark.navigationAll;
+    //     for (let item of navigationList){
+    //         if (item.functionId != 'index' || item.functionId != 'cart' || item.functionId != 'home') {
+    //             continue;
+    //         }
+    //         newNavigationList.push(item);
+    //     }
+    //
+    //     obj.result.modeMap.dark.navigationAll = newNavigationList;
+    // }
 
     // 底部导航栏   普通模式
     if (obj?.result?.modeMap?.normal?.navigationAll.length > 0) {
@@ -28,7 +28,7 @@ if (url.includes("functionId=readCustomSurfaceList")) {
             newNavigationList.push(item);
         }
 
-        obj.result.modeMap.normal.navigationAll = newNavigationList;
+        // obj.result.modeMap.normal.navigationAll = newNavigationList;
     }
 }
 
