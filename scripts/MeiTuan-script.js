27e823343309c387da7c8430725a62c9419ccd25
previous <2831207404@qq.com>
2025-03-09 11:52:31 +0800

    优化美团去广告脚本

--- scripts/MeiTuan-script.js
+++ scripts/MeiTuan-script.js
@@ -2,48 +2,38 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("/miniprogram/index/aggregate")) {
-    // 顶部功能栏
-    // if (obj?.data?.jgArea?.resources?.length > 0) {
-    //     let oldList = obj.data.jgArea.resources;
-    //     let newList = [];
-    //     for (let item of oldList){
-    //         if (item.resourceId == '636398'
-    //         || item.resourceId == '635778'
-    //         || item.resourceId == '635793'
-    //         || item.resourceId == '636215'
-    //         || item.resourceId == '636222') {
-    //             newList.push(item);
-    //         }
-    //     }
-    //
-    //     obj.data.jgArea.resources = newList;
-    // }
-
-    // 首页推荐栏
-    obj.data.secKillingArea = {};
-    obj.data.feed = {};
-    obj.data.bizArea = {};
-}
 
 // 底部导航栏
-if (url.includes("/miniprogram/tabbar/all/query ")) {
-    if (obj?.data?.length > 0) {
-        let oldList = obj.data;
+if (url.includes("/user/functions/list ")) {
+    if (obj?.data?.category_list?.length > 0) {
+        let oldList = obj.data.category_list;
         let newList = [];
-        for (let item of oldList){
-            if (item.path.includes('mt/mt')
-                || item.path.includes('tabs/index')
-                || item.path.includes('mine/mine')) {
+        for (let item of oldList) {
+            if (item.cat_title.includes('我的')){
                 newList.push(item);
             }
         }
-        obj.data = newList;
+        obj.data.category_list = newList;
     }
+
+
+    if (obj?.data?.category_listIterator?.length > 0) {
+        let oldList = obj.data.category_listIterator;
+        let newList = [];
+        for (let item of oldList) {
+            if (item.cat_title.includes('我的')){
+                newList.push(item);
+            }
+        }
+        obj.data.category_listIterator = newList;
+    }
+
+    obj.data.my_favorite = {};
+
 }
 
 
-    $done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(obj)});
 
 
 
