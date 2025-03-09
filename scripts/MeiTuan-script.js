0b315a305ca9a946d4579ca3251a14dd565d6a5d
previous <2831207404@qq.com>
2025-03-09 11:27:39 +0800

    优化美团去广告脚本

--- scripts/MeiTuan-script.js
+++ scripts/MeiTuan-script.js
@@ -4,28 +4,25 @@ let obj = JSON.parse($response.body);
 
 if (url.includes("/miniprogram/index/aggregate")) {
     // 顶部功能栏
-    if (obj?.data?.jgArea?.resources?.length > 0) {
-        let oldList = obj.data.jgArea.resources;
-        let newList = [];
-        for (let item of oldList){
-            if (item.resourceId == '636398'
-            || item.resourceId == '635778'
-            || item.resourceId == '635793'
-            || item.resourceId == '636215'
-            || item.resourceId == '636222') {
-                newList.push(item);
-            }
-        }
+    // if (obj?.data?.jgArea?.resources?.length > 0) {
+    //     let oldList = obj.data.jgArea.resources;
+    //     let newList = [];
+    //     for (let item of oldList){
+    //         if (item.resourceId == '636398'
+    //         || item.resourceId == '635778'
+    //         || item.resourceId == '635793'
+    //         || item.resourceId == '636215'
+    //         || item.resourceId == '636222') {
+    //             newList.push(item);
+    //         }
+    //     }
+    //
+    //     obj.data.jgArea.resources = newList;
+    // }
 
-        obj.data.jgArea.resources = newList;
-    }
-
-    // 首页推荐栏1
-    if (obj?.data?.feed?.items?.length > 0) {
-        obj.data.feed.items = [];
-    }
-
+    // 首页推荐栏
     obj.data.secKillingArea = {};
+    obj.data.feed = {};
     obj.data.bizArea = {};
 }
 
