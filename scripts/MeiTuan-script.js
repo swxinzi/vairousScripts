6f35fa40d04af0e6ff55be02ad0ec9f786430746
previous <2831207404@qq.com>
2025-03-09 11:24:56 +0800

    增加美团去广告脚本

---
+++ scripts/MeiTuan-script.js
@@ -0,0 +1,34 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("/miniprogram/index/aggregate")) {
+    // 顶部功能栏
+    if (obj?.data?.jgArea?.resources?.length > 0) {
+        let oldList = obj.data.jgArea.resources;
+        let newList = [];
+        for (let item of oldList){
+            if (item.resourceId == '636398'
+            || item.resourceId == '635778'
+            || item.resourceId == '635793'
+            || item.resourceId == '636215'
+            || item.resourceId == '636222') {
+                newList.push(item);
+            }
+        }
+
+        obj.data.jgArea.resources = newList;
+    }
+
+    // 首页推荐栏1
+    if (obj?.data?.feed?.items?.length > 0) {
+        obj.data.feed.items = [];
+    }
+
+    obj.data.secKillingArea = {};
+    obj.data.bizArea = {};
+}
+
+
+$done({ body: JSON.stringify(obj) });
+
